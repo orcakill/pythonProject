6b309e4b01796ffc11f84cd5b8ee352391c61526
orcakill <orcakill@dingtalk.com>
2022-05-06 19:08:59 +0800

    1、python基础语法

--- runoob/1、python基础语法.py
+++ runoob/1、python基础语法.py
@@ -8,4 +8,18 @@ print(str[0:-1])
 print(str[0])
 # 输出字符串第3个到第6个字符
 print(str[2:5])
+# 输出第三个字符以后的字符
 print(str[2:])
+# 输出第第二个到第六个个字符之间的字符，步长为2
+print(str[1:5:2])
+# 输出字符串两次
+print(str*2)
+# 输出字符串拼接
+print(str+"2")
+
+print('------------------------------')
+
+# 输出是否转译的区别
+print("hello\n run")
+print(r"hello\n run")
+
